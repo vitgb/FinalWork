02349ba03aa731fa588b2cf0da996afd66391cfc
unknown <v1599199@gmail.com>
2023-01-22 22:26:46 +0300

    Added a method array conversion

--- Program.cs
+++ Program.cs
@@ -13,18 +13,50 @@ string [] CreateArray1 (int size)
     return Array1;
 }
 
-void ShowArray (string[] Array1)
+void ShowArray (string[] Array)
 
 {
-    for(int i = 0; i < Array1.Length; i++)
+    for(int i = 0; i < Array.Length; i++)
         
-        Console.Write("[" + Array1[i] + "]" + " " );
+        Console.Write("[" + Array[i] + "]" + " " );
 
     Console.WriteLine();
 }
+
+string [] CreateArrayThreeSymbol (string[] Array1)
+{
+    
+    int count = 0;
+    string [] Array2 = new string [Array1.Length];
+    for (int i = 0; i < Array1.Length; i++)
+    {
+        if (Array1[i].Length <= 3)
+        {
+            Array2[count] = Array1[i];
+            count++;
+        }
+    }
+    return Array2;
+}
+
+void ShowArray2 (string[] Array2)
+
+{
+    for(int i = 0; i < Array2.Length; i++)
+        
+        Console.Write("[" + Array2[i] + "]" + " " );
+
+    Console.WriteLine();
+}
+
+
 
 Console.Write("Enter the number of words: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
 string [] ArrayUser = CreateArray1 (n);
 ShowArray (ArrayUser);
+Console.WriteLine();
+
+ShowArray (CreateArrayThreeSymbol(ArrayUser));
+
